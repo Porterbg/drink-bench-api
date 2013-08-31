934bfc0505e0aff388ef586533e01071ffd1302a
Daniel <Drgreen_bg@yahoo.com>
2013-08-31 06:35:48 -0700

    full upload

---
+++ DrinkBench/DrinkBench.Models/MessageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkBench.Models
+{
+    public enum MessageType
+    {
+        Normal = 0,
+        Invitation = 1,
+        Readed = 2
+    }
+}
